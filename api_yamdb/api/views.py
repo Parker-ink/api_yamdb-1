d013c75b7ca72453160109bfbc841980202e1af4
Евгений Цуран <tsurik_01@mail.ru>
2022-10-05 02:03:39 +0300

    создал сериализаторы, вьюхи, урлы

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from reviews.models import (
+    Category,
+    Genre,
+    Title
+)
+from .serializers import (
+    CategorySerializer,
+    GenreSerializer,
+    TitleSerializer
+)
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,3 @@
-from email.policy import default
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
