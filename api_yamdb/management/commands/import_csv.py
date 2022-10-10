f8eb19ffd80ad57c3f78cd97abb5d82edb3e7927
afoninsb <88446692+afoninsb@users.noreply.github.com>
2022-10-10 19:14:58 +0300

    Update import_csv.py

--- api_yamdb/management/commands/import_csv.py
+++ api_yamdb/management/commands/import_csv.py
@@ -11,7 +11,11 @@ class Command(BaseCommand):
     help = u'Загрузка информации из csv-файла в базу данных'
 
     def add_arguments(self, parser):
-        parser.add_argument('file_name', type=str, help=u'Имя файла без расширения')
+        parser.add_argument(
+            'file_name',
+            type=str,
+            help=u'Имя файла без расширения'
+        )
 
     def handle(self, *args, **kwargs):
         file_name = f"{kwargs['file_name']}.csv"
