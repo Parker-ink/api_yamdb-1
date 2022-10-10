3b4f0bd52ad27fc752556403e2eb356f5b354db6
Вадим Ковалев <minenikolasspace@bk.ru>
2022-10-10 15:22:55 +0300

    All Tests Done

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_filters', 
+    'django_filters',
     'api.apps.ApiConfig',
     'reviews.apps.ReviewsConfig',
 ]
