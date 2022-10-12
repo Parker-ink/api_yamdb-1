2e4e86c848f2c136c6f47cd40258b651880a8ef8
Sergey Afonin <afoninsb@yandex.ru>
2022-10-12 16:14:34 +0300

    admin-panel

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_staff',
+        'bio',
+        'role',
+        'is_active'
+    )
+    list_filter = ('is_active', 'role')
+    search_fields = ('username', 'email')
