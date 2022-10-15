1ede3445a73d505e2b7d7e86fa25178afb7cf664
Sergey Afonin <afoninsb@yandex.ru>
2022-10-15 15:54:48 +0300

    Прокомментировал файл import_csv

--- api_yamdb/reviews/management/commands/import_csv.py
+++ api_yamdb/reviews/management/commands/import_csv.py
@@ -12,6 +12,7 @@ class Command(BaseCommand):
     help = '''Загрузка тестовой информации из csv-файла в базу данных.
     Вся существующая информация будет удалена из базы данных.'''
 
+    # имя файла с данными, приложение, модель
     DATA = (
         ('users.csv', 'users', 'User'),
         ('category.csv', 'reviews', 'Category'),
@@ -24,10 +25,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         message = 'Данные добавлены!'
+
+        # Если БД существует, очищаем её таблицы от данных
         with contextlib.suppress(ValueError):
             call_command('flush', '--no-input')
+
+        # Выполняем миграции
         call_command('migrate')
+
+        # Импорт данных из файлов в БД
         for fixture, app, model in self.DATA:
+
+            # Импортрируем модель
             try:
                 current_model = apps.get_model(app, model)
             except LookupError:
@@ -35,6 +44,8 @@ class Command(BaseCommand):
                            f'Ошибка в наименованиях приложений и моделей: '
                            f'{app}, {model}')
                 break
+
+            # Получаем полный путь до файла с данными
             path_to_file = os.path.join(
                 settings.BASE_DIR, 'static', 'data', fixture
             )
@@ -42,8 +53,16 @@ class Command(BaseCommand):
                 message = (f'Данные не добавлены!!! '
                            f'Такой файл не существует: {path_to_file}')
                 break
+
             with open(path_to_file, newline='') as csvfile:
+
+                # Получаем данные из csv файла
                 reader = csv.DictReader(csvfile)
+
+                # Формируем список данных
                 bulk_data = [current_model(**row) for row in reader]
+
+                # Сохраняем данные в БД
                 current_model.objects.bulk_create(bulk_data)
+
         self.stdout.write(message)
